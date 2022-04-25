b3ee530ed59b9ae81d458c325fe0a59492e361fd
xRockmetalx <rockmetal281@gmail.com>
2022-04-25 05:57:03 -0400

    add sha224, sha384 to SHA

--- include/utils/crypto/sha.h
+++ include/utils/crypto/sha.h
@@ -17,7 +17,9 @@ public:
     SHA& operator = (const SHA &&) = delete;
     ~SHA() = delete;
 
+    static std::string sha224(const std::string &str);
     static std::string sha256(const std::string &str);
+    static std::string sha384(const std::string &str);
     static std::string sha512(const std::string &str);
 };
 
--- src/utils/crypto/sha.cpp
+++ src/utils/crypto/sha.cpp
@@ -7,6 +7,17 @@
 
 #include <utils/crypto/convert.h>
 
+std::string SHA::sha224(const std::string &str) {
+    SHA256_CTX context;
+    unsigned char buffer[ SHA224_DIGEST_LENGTH ];
+
+    SHA224_Init(&context);
+    SHA224_Update(&context, str.c_str(), str.size());
+    SHA224_Final(buffer, &context);
+
+    return Convert::to_hexadecimal(std::string{ buffer, buffer + SHA224_DIGEST_LENGTH });
+}
+
 std::string SHA::sha256(const std::string &str) {
     SHA256_CTX context;
     unsigned char buffer[ SHA256_DIGEST_LENGTH ];
@@ -16,6 +27,17 @@ std::string SHA::sha256(const std::string &str) {
     SHA256_Final(buffer, &context);
 
     return Convert::to_hexadecimal(std::string{ buffer, buffer + SHA256_DIGEST_LENGTH });
+}
+
+std::string SHA::sha384(const std::string &str) {
+    SHA512_CTX context;
+    unsigned char buffer[ SHA384_DIGEST_LENGTH ];
+
+    SHA384_Init(&context);
+    SHA384_Update(&context, str.c_str(), str.size());
+    SHA384_Final(buffer, &context);
+
+    return Convert::to_hexadecimal(std::string{ buffer, buffer + SHA384_DIGEST_LENGTH });
 }
 
 std::string SHA::sha512(const std::string &str) {
